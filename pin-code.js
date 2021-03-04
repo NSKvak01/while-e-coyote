3a5d54363312fb23e137ff93edbddcec4e2e07be
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-03-04 14:28:29 -0500

    Adds code.

--- pin-code.js
+++ pin-code.js
@@ -1,3 +1,17 @@
 const getInput = require('./get-input.js');
 
 // Your code here!
+let n = 1
+let sum = 0
+let userGuessCor = false;
+while (getInput(n) !== undefined){
+    sum++
+    n++
+    if (getInput(n) === '1234'){
+        userGuessCor = true;
+        console.log('That is correct. You took', sum+1, 'guess(es).')
+    }
+}   
+if (userGuessCor === false){
+    console.log('None of the entered pin codes are correct.')
+}
--- sum-args.js
+++ sum-args.js
@@ -1,3 +1,9 @@
 const getInput = require('./get-input.js');
 
 // Your code here!
+let n = 1
+let sum = 0
+while (getInput(n) !== undefined) {
+    sum += parseInt(getInput(n))
+    n++
+} console.log(sum)
